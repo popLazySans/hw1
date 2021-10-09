32792e4c13cb10e84f18ec5cb72a529c2d4c7904
IamPimdedd <hasamu.44@gmail.com>
2021-10-09 20:31:39 +0700

    no message

---
+++ Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Student : Person
+{
+    private string studentID;
+
+    public Student(string name, string address, string citizenID, string studentID)
+    : base(name, address, citizenID)
+    {
+        this.studentID = studentID;
+    }
+
+}
---
+++ Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Teacher : Person
+{
+    private string employeeID;
+
+    public Teacher(string name, string address, string citizenID, string employeeID)
+    : base(name, address, citizenID)
+    {
+        this.employeeID = employeeID;
+    }
+
+}
